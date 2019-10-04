e2f1acb4e5c884b69e76998cfbc07a43892ae9f5
Charlie Chiu <charliechiu@outlook.com>
2019-10-04 01:21:21 +0000

    add all ajax call methods and screen, added pages and started impl server connection, added firebasex, push notification works

--- www/js/index.js
+++ www/js/index.js
@@ -1,66 +1,193 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+var pendingPetId = "";
+var newPetName = "";
+var newPetImages = [];
+var imagesUploaded = 0;
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+// deviceready Event Handler
+function onDeviceReady() {
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+    if(typeof window.FirebasePlugin !== "undefined"){
+        window.FirebasePlugin.subscribe("newpet");
+        window.FirebasePlugin.getToken(function(token) {
+            console.log(token);
+            // Send the token to our slack channel via HTTP POST request
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", "https://hooks.slack.com/services/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", true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify({
+                text: "Hey hey, that's a new App installed, and this is the token for push me notifications directly: " + token
+            }));
+        }, function(error) {
+            console.error(error);
+        });    
     }
-};
+    
+    checkPendingPets();
 
-app.initialize();
+    // click buttons listeners
+    document.getElementById("unlockAndGoToUpload").addEventListener ("click", unlockAndGoToUpload); 
+    document.getElementById("denyPet").addEventListener ("click", denyPet); 
+    document.getElementById("cameraTakePicture").addEventListener ("click", cameraTakePicture); 
+    document.getElementById("errorBackToHome").addEventListener ("click", checkPendingPets); 
+    document.getElementById("thanksBackToHome").addEventListener ("click", checkPendingPets); 
+    document.getElementById("startUpload").addEventListener ("click", startUpload); 
 
-// take picture from camera
-document.getElementById("cameraTakePicture").addEventListener 
-   ("click", cameraTakePicture); 
-   
-   function cameraTakePicture() { 
+}
+
+// Check Pending Pets function
+function checkPendingPets() {
+    $('.sectionpage').hide();
+    $.getJSON("http://35.244.89.241/pendingpet.php", function(data) {
+        console.log(data);
+        if(data.length > 0){
+            $('#home-pet').show();
+            pendingPetId = data[0].id;
+            $('#pendingpet-img').attr('src', 'data:image/jpeg;base64,' + data[0].file)
+        } else {
+            $('#home-nopet').show();
+        }
+    }).fail(function(error) {
+        console.error(error);
+        $('#home-nopet').show();
+    });
+}
+
+function unlockAndGoToUpload() {
+
+    $.getJSON("http://35.244.89.241/opendoor.php", function(data) {
+        console.log(data);
+    }).fail(function(error) {
+        console.error(error);
+    });
+
+    $.getJSON("http://35.244.89.241/deletependingpet.php?filename=" + pendingPetId, function(data) {
+        console.log(data);
+    }).fail(function(error) {
+        console.error(error);
+    });
+
+    $('.sectionpage').hide();
+    $('#upload-pet').show();
+}
+
+function denyPet() {
+
+    $.getJSON("http://35.244.89.241/deletependingpet.php?filename=" + pendingPetId, function(data) {
+        console.log(data);
+    }).fail(function(error) {
+        console.error(error);
+    });
+
+    checkPendingPets();
+}
+
+function goToUploadSuccess() {
+    $('.sectionpage').hide();
+    $('#upload-success').show();
+}
+
+function cameraTakePicture() {
     navigator.camera.getPicture(onSuccess, onFail, { quality: 50,
         destinationType: Camera.DestinationType.DATA_URL,
         sourceType: Camera.PictureSourceType.PHOTOLIBRARY
-     });
-  
-     function onSuccess(imageURL) {
+    });
+
+    function onSuccess(imageURL) {
         var image = document.getElementById('myImage');
         image.src = imageURL;
-     }
-  
-     function onFail(message) {
+    }
+
+    function onFail(message) {
         alert('Failed because: ' + message);
-     }
- }
+    }
+}
+
+function cameraTakePicture() {
+    window.imagePicker.getPictures(
+        function(results) {
+            newPetImages = [];
+
+            if(results.length > 0){
+                $('.upload-example').hide();
+                $('#startUpload').show();
+
+                for (var i = 0; i < results.length; i++) {
+                    console.log('Image URI: ' + results[i]);
+                    newPetImages.push(results[i]);
+                    $('#upload-pictures').append('<img class="petuploadimg" src="'+results[i]+'">');
+                }
+
+            } else {
+                $('.upload-example').show();
+                $('#upload-pictures').html('');
+                $('#startUpload').hide();
+            }
+
+        }, function (error) {
+            console.error(error);
+        }, {
+            width: 800,
+            quality: 80
+        }
+    );
+}
+    
+function startUpload(){
+    navigator.notification.prompt(
+        'Please enter your pet\'s name',
+        uploadImages,
+        'Pet Name',
+        ['SAVE'],
+        ''
+    );
+}
+
+function uploadImages(results) {   
+    if(results.input1.length > 0){
+        newPetName = results.input1.replace(/\s/g,'');
+
+        for (var i = 0; i < newPetImages.length; i++) {
+            window.plugins.Base64.encodeFile(newPetImages[i], function(base64image) {
+    
+                $.ajax({
+                    type: "POST",
+                    url: "http://35.244.89.241/newpet.php",
+                    data: { petname: newPetName, petimage: base64image }
+                }).done(function() {
+                    imagesUploaded++;
+                    checkUploadComplete();
+                });
+    
+            }, function(error){
+                console.error(error);
+            });
+        }
+
+    } else {
+        alert("Sorry, a name for your pet is mandatory");
+        startUpload();
+    }
+}
+
+function checkUploadComplete() {
+    if(imagesUploaded < newPetImages.length) return;
+    if(imagesUploaded == newPetImages.length){
+        startRetrain();
+        imagesUploaded = 0;
+        newPetImages = [];
+        $('.upload-example').show();
+        $('#upload-pictures').html('');
+        $('#startUpload').hide();
+        goToUploadSuccess();
+    }
+}
+
+function startRetrain() {
+    $.getJSON("http://35.244.89.241/startretrain.php", function(data) {
+        console.log(data);
+    }).fail(function(error) {
+        console.error(error);
+    });
+}
+
+document.addEventListener("deviceready", onDeviceReady, false);
